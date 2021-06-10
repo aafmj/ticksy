9386f1deaa842620f5d57a422ea1e8ee8e159a70
Amirali <amiralifmj@gmail.com>
2021-06-10 20:31:22 +0430

    customize admin

--- ticketing/admin.py
+++ ticketing/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from .models import Topic, Ticket, Message, Attachment
 
-# Register your models here.
+
+class AttachInline(admin.TabularInline):
+    model = Attachment
+    extra = 1
+
+
+class MessageInline(admin.StackedInline):
+    model = Message
+    extra = 1
+    show_change_link = True
+
+
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'creator')
+    search_fields = ['title', 'description', 'slug', 'creator']
+    list_filter = []
+    filter_horizontal = ['supporters']
+    autocomplete_fields = ['creator']
+
+
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('title', 'get_status_display', 'get_priority_display', 'last_update')
+    search_fields = ['title', 'creator']
+    list_filter = ['status', 'priority', 'creation_date', 'last_update', 'topic']
+    readonly_fields = ['creation_date', 'last_update']
+    inlines = [MessageInline]
+    autocomplete_fields = ['creator', 'topic']
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('get_short_text', 'user', 'ticket', 'rate')
+    search_fields = ['text', 'user']
+    list_filter = ['date', 'rate']
+    inlines = [AttachInline]
+    readonly_fields = ['date']
+    autocomplete_fields = ['user', 'ticket']
+
+
+# class AttachmentAdmin(admin.ModelAdmin):
+#     list_display  = ('message', 'file')
+#     list_filter   = []
+
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Ticket, TicketAdmin)
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Attachment)
+# admin.site.register(Attachment, AttachmentAdmin)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
 
-# Register your models here.
+from .models import User, Identity
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    search_fields = ['email', 'first_name', 'last_name']
+    readonly_fields = ('date_joined', 'last_login')
+    fieldsets = (
+        (None, {'fields': ('email', 'first_name', 'last_name', 'avatar', 'code', 'password', 'is_active')}),
+        (_('important dates'), {'fields': ('date_joined', 'last_login')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'first_name', 'last_name', 'avatar', 'code'),
+        }),
+    )
+
+
+class IdentityAdmin(admin.ModelAdmin):
+    list_display = ('user', 'status', 'request_time', 'expire_time')
+    list_filter = ['status', 'request_time', 'expire_time']
+    search_fields = ['user', 'email']
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Identity, IdentityAdmin)
